7a346e3992287cf8b4aaa69c42ccf609b2158846
Sozinov Alexander <sanzeting@mail.ru>
2018-07-30 19:46:08 +0300

    MINT-1702 Wizard pages, pages switch, navigateCallbacks

--- src/components/dialogs/UpdateProvideDialog/UpdateProfileDialog.jsx
+++ src/components/dialogs/UpdateProvideDialog/UpdateProfileDialog.jsx
@@ -10,7 +10,6 @@ import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
 import { TextField } from 'redux-form-material-ui'
 import { connect } from 'react-redux'
-import { ACCEPT_IMAGES } from '@chronobank/core/models/FileSelect/FileExtension'
 import { DUCK_SESSION, updateUserProfile } from '@chronobank/core/redux/session/actions'
 import { getAccountProfileSummary } from '@chronobank/core/redux/session/selectors'
 import {
